b4862cfa947abd742c3df2c5ef498c847d2ee2e7
vo <1929750203@qq.com>
2025-12-07 12:59:49 +0800

    init

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -1,85 +1,37 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <div id="app">
+    <input type="number" v-model="numberValue" />
+    <button @click="addNumber(Number(numberValue))">Add Number</button>
+    <button @click="showNumbers">Show Numbers</button>
+    <div v-for="(number, index) in numberList" :key="index">
+      {{ number }}
     </div>
-  </header>
-
-  <RouterView />
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<script setup lang="ts">
+import { ref } from 'vue';
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+const numberValue = ref<number>(0);
+const numberList = ref<number[]>([]);
+const show = ref(false);
+const useStore = () => {
+  const cache: number[] = [];
+  const addNumber = (num: number) => {
+    cache.push(num);
+  }
+  const getNumbers = () => {
+    return [...cache];
+  }
+  return {
+    addNumber,
+    getNumbers
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
 }
-</style>
+const { addNumber, getNumbers } = useStore();
+const showNumbers = () => {
+  show.value = true;
+  numberList.value = getNumbers();
+}
+</script>
